bb9de584be62015fdfd63e8f39a0a9eda9b12c59
DESKTOP-7RP6RR3\dipto <diptokmk47@outlook.com>
2018-03-19 13:57:59 +0600

    Change job titles in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "solve problems."
+				"am a Python Developer "," am a Full Stack Web Developer ","  am a Desktop Application Developer", "love everything about code.", "A problem solver"
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
